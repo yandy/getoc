4f64b64292392153c95ab877edc0440ae6ecc6bd
Michael Ding <yandy.ding@gmail.com>
2015-09-16 19:11:52 +0800

    fix dup table bug

--- getoc/spiders/oc163.py
+++ getoc/spiders/oc163.py
@@ -16,7 +16,8 @@ class Oc163Spider(scrapy.Spider):
         course_name = ''.join(response.css('.mainwrap h2::text').extract())
         course = CourseItem(name=course_name)
         yield course
-        for idx, elem in enumerate(response.css('table.m-clist td.u-ctitle a')):
+        table = response.css('table.m-clist')[-1]
+        for idx, elem in enumerate(table.css('td.u-ctitle a')):
             lesson = LessonItem(idx=(idx+1))
             lesson['title'] = ''.join(elem.xpath('text()').extract())
             lesson['url'] = ''.join(elem.xpath('@href').extract())
